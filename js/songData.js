1c0a476678bbf40e718f997789f657322e13f321
seohyunCho <shanny06@naver.com>
2023-10-25 23:33:55 +0900

    노래 데이터 추가

---
+++ js/songData.js
@@ -0,0 +1,128 @@
+const songData = [
+    {
+        title: "흐린 기억 속의 그대",
+        singer: "현진영",
+        audio: "../songs/흐린 기억 속의 그대 - 현진영.mp3",
+        image: "../img/song-img/흐린 기억 속의 그대.png"
+    },
+    {
+        title: "DOC와 함께 춤을",
+        singer: "DJ DOC",
+        audio: "../songs/DOC와 함께 춤을 - DJ DOC.mp3",
+        image: "../img/song-img/DOC와 함께 춤을.png"
+    },
+    {
+        title: "난 알아요",
+        singer: "서태지와 아이들",
+        audio: "../songs/난 알아요 - 서태지와 아이들.mp3",
+        image: "../img/song-img/난 알아요.png"
+    },
+    {
+        title: "애상",
+        singer: "쿨",
+        audio: "../songs/애상 - 쿨.mp3",
+        image: "../img/song-img/애상.png"
+    },
+    {
+        title: "날개 잃은 천사",
+        singer: "룰라",
+        audio: "../songs/날개 잃은 천사 - 룰라.mp3",
+        image: "../img/song-img/날개 잃은 천사.png"
+    },
+    {
+        title: "그대에게",
+        singer: "신해철",
+        audio: "../songs/그대에게 - 신해철.mp3",
+        image: "../img/song-img/그대에게.png"
+    },
+    {
+        title: "잘못된 만남",
+        singer: "김건모",
+        audio: "../songs/잘못된 만남 - 김건모.mp3",
+        image: "../img/song-img/잘못된 만남.png"
+    },
+    {
+        title: "이별공식",
+        singer: "Ref",
+        audio: "../songs/이별공식 - Ref.mp3",
+        image: "../img/song-img/이별공식.png"
+    },
+    {
+        title: "파도",
+        singer: "유엔(UN)",
+        audio: "../songs/파도 - UN.mp3",
+        image: "../img/song-img/파도.png"
+    },
+    {
+        title: "버스 안에서",
+        singer: "자자",
+        audio: "../songs/버스 안에서 - 자자.mp3",
+        image: "../img/song-img/버스 안에서.png"
+    },
+    {
+        title: "나를 돌아봐",
+        singer: "듀스",
+        audio: "../songs/나를 돌아봐 - 듀스.mp3",
+        image: "../img/song-img/나를 돌아봐.png"
+    },
+    {
+        title: "뿌요뿌요",
+        singer: "유피",
+        audio: "../songs/뿌요뿌요 - 유피.mp3",
+        image: "../img/song-img/뿌요뿌요.png"
+    },
+    {
+        title: "널 사랑하니까",
+        singer: "신승훈",
+        audio: "../songs/널 사랑하니까 - 신승훈",
+        image: "../img/song-img/널 사랑하니까.png"
+    },
+    {
+        title: "멍",
+        singer: "김현정",
+        audio: "../songs/멍 - 김현정.mp3",
+        image: "../img/song-img/멍.png"
+    },
+    {
+        title: "Never Ending Story",
+        singer: "부활",
+        audio: "../songs/Never Ending Story - 부활.mp3",
+        image: "../img/song-img/Never Ending Story.png"
+    },
+    {
+        title: "내 눈물 모아",
+        singer: "서지원",
+        audio: "../songs/내 눈물 모아 - 서지원.mp3",
+        image: "../img/song-img/내 눈물 모아.png"
+    },
+    {
+        title: "아름다운 구속",
+        singer: "김종서",
+        audio: "../songs/아름다운 구속 - 김종서.mp3",
+        image: "../img/song-img/아름다운 구속.png"
+    },
+    {
+        title: "순정",
+        singer: "코요태",
+        audio: "../songs/순정 코요태.mp3",
+        image: "../img/song-img/순정.png"
+    },
+    {
+        title: "어젯밤 이야기",
+        singer: "소방차",
+        audio: "../songs/어젯밤 이야기 - 소방차.mp3",
+        image: "../img/song-img/어젯밤 이야기.png"
+    },
+    {
+        title: "하여가",
+        singer: "서태지와 아이들",
+        audio: "../songs/하여가 - 서태지와 아이들.mp3",
+        image: "../img/song-img/하여가.png"
+    },
+    {
+        title: "White Love",
+        singer: "터보",
+        audio: "../songs/White Love - 터보.mp3",
+        image: "../img/song-img/White Love.png"
+    },
+]
