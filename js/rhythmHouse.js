04ea23f12818d49242f16a0e636fe9ea6493d7a6
seohyunCho <shanny06@naver.com>
2023-10-25 21:42:03 +0900

    음악 파일 생성

---
+++ js/rhythmHouse.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    // recordCircleRotate();
+    let musicTitle = "난 알아요"
+    let musicSinger = "서태지와 아이들"
+    let musicInfo = document.getElementById('music-info');
+    musicInfo.innerText = `${musicTitle} - ${musicSinger}`;
+}
+    
+// function recordCircleRotate() {
+
+// }
