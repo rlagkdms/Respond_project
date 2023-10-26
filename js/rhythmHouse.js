9823cbcd47dc0ecae21daa308a3149afcc7fe534
seohyunCho <shanny06@naver.com>
2023-10-26 23:14:45 +0900

    음악 재생 및 플레이리스트 기능 구현

--- js/rhythmHouse.js
+++ js/rhythmHouse.js
@@ -1,11 +1,33 @@
+const playBtn = document.getElementById('pause-song');
+const prevBtn = document.getElementById('before-song');
+const nextBtn = document.getElementById('next-song');
+const audio = document.getElementById('audio');
+const recordCircle1 = document.getElementsByClassName('record-circle')[0];
+const recordCircle2 = document.getElementsByClassName('record-circle')[1];
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById('progress-container');
+
+audio.addEventListener('ended', playNextSong);
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', changeProgress);
+
+let songIndex = 0;
+
 window.onload = function() {
-    let musicTitle = "난 알아요"
-    let musicSinger = "서태지와 아이들"
-    let musicInfo = document.getElementById('music-info');
-    musicInfo.innerText = `${musicTitle} - ${musicSinger}`;
+    loadSong(songData[songIndex]);
+
     constructPlaylist();
+    playBtn.style.fontSize = "32px";
 }
     
+function loadSong(song){
+    audio.src = song.audio;
+    let musicTitle = songData[songIndex].title;
+    let musicSinger = songData[songIndex].singer;
+    let musicInfo = document.getElementById('music-info');
+    musicInfo.innerText = `${musicTitle} - ${musicSinger}`;
+}
+
 function constructPlaylist() {
     const playlistDiv = document.getElementById('music-list-div');
     for(let data of songData){
@@ -19,7 +41,6 @@ function constructPlaylist() {
         let imageDiv = document.createElement('div');
         imageDiv.className = "song-image";
         imageDiv.innerHTML = `<img src="${image}" />`
-        console.log(imageDiv.innerHTML);
 
         let songProfile = document.createElement('div');
         songProfile.className = 'song-profile';
@@ -46,4 +67,66 @@ function constructPlaylist() {
 
         playlistDiv.appendChild(songDiv);
     }
+}
+
+function playSong(){
+    const isPlaying = audio.classList.contains('play');
+    if(isPlaying){
+        pauseMusic();
+    }else{
+        playMusic();
+    }
+}
+
+function playPrevSong(){
+    songIndex--;
+
+    if(songIndex < 0){
+        songIndex = songData.length - 1;
+    }
+    loadSong(songData[songIndex]);
+    playMusic();
+}
+
+function playNextSong(){
+    songIndex++;
+    
+    if(songIndex > songData.length - 1){
+        songIndex = 0;
+    }
+
+    loadSong(songData[songIndex]);
+    playMusic();
+}
+
+function pauseMusic(){
+    audio.pause();
+    playBtn.innerHTML = `<iconify-icon icon="mdi:play"></iconify-icon>`;
+    playBtn.style.fontSize = "32px";
+    audio.classList.remove('play');
+    recordCircle1.style.animation = "none";
+    recordCircle2.style.animation = "none";
+    // animation: rotate_image 2s linear infinite;
+}
+
+function playMusic(){
+    audio.play();
+    playBtn.innerHTML = `<iconify-icon icon="bytesize:pause" id="pause-song"></iconify-icon>`;
+    playBtn.style.fontSize = "20px";
+    audio.classList.add('play');
+    recordCircle1.style.animation = "rotate_image 2s linear infinite";
+    recordCircle2.style.animation = "rotate_image 2s linear infinite";
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement;
+    const progressPer = (currentTime / duration) * 100;
+    progress.style.width = `${progressPer}%`;
+}
+
+function changeProgress(e){
+    const width = e.target.clientWidth;
+    const offsetX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (offsetX / width) * duration;
 }
